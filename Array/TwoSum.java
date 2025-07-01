61424efb851f3ed4a7b5bc5ff348ef669c146089
Prawin Kumar Mandal <prawinkumar564@gmail.com>
2025-07-01 23:09:48 +0530

    Added number of TwoSum

---
+++ Array/TwoSum.java
@@ -0,0 +1,9 @@
+package Array;
+public class TwoSum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
